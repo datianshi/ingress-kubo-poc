46dc61334bee0704ffdcf90c79e91ebd12ae75d5
Shaozhen Ding <dsz0111@gmail.com>
2018-10-04 14:14:43 -0500

    add ingress nsx-t

--- install-music.sh
+++ install-music.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+
+#Create the secrets for ssl
 kubectl apply -f $1/namespace.yml
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /tmp/tls.key -out /tmp/tls.crt -subj "/CN=$1.nsx-t.shaozhenpcf.com"
+
+kubectl create secret tls $1-secret --key /tmp/tls.key --cert /tmp/tls.crt -n $1
 kubectl apply -f $1/music.yml
 kubectl apply -f "$1/ingress-$2.yml"
