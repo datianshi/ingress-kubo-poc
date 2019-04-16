49012c1b9b3fb6352d196a780dd6f67cb527b292
Shaozhen Ding <dsz0111@gmail.com>
2019-04-16 08:45:29 -0500

    update

--- install-music.sh
+++ install-music.sh
@@ -2,7 +2,7 @@
 
 #Create the secrets for ssl
 kubectl apply -f $1/namespace.yml
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /tmp/tls.key -out /tmp/tls.crt -subj "/CN=$1.nsx-t.shaozhenpcf.com"
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /tmp/tls.key -out /tmp/tls.crt -subj "/CN=music.pks.hulk.pvd.pez.pivotal.io"
 
 kubectl create secret tls $1-secret --key /tmp/tls.key --cert /tmp/tls.crt -n $1
 kubectl apply -f $1/music.yml
--- music1/install_certificate.sh
+++ music1/install_certificate.sh
@@ -1,3 +1,3 @@
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /tmp/tls.key -out /tmp/tls.crt -subj "/CN=$1.nsx-t.shaozhenpcf.com"
 
-kubectl create secret tls music1-secret --key /tmp/tls.key --cert /tmp/tls.crt -n $music1
+kubectl create secret tls music1-secret --key /tmp/tls.key --cert /tmp/tls.crt -n $1
